93346b2e7c9afec5cb2326ffabf74b8aab149ef1
boopathytekh <boopathytekh@gmail.com>
2017-06-19 15:37:36 +0530

    greatest among the three numbers
    three numbers

---
+++ greatest.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("enter the three numbers");
+scanf("%d%d%d",&a,&b,&c);
+
+if(a>b)&&(a>c)
+printf("a is greater");
+if(b>c)&&(b>a)
+printf("b is greater");
+if(c>b)&&(c>a)
+printf(" c is greater");
+getch();
+}
+
