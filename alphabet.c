fb8ffa9fa43d7a07f366de69dc294c64da3a3ec3
boopathytekh <boopathytekh@gmail.com>
2017-06-19 14:13:40 +0530

    alphabet
    c program

---
+++ alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+ch a;
+clrscr();
+printf("enter a alphabet");
+scanf("%c",&a);
+if(a>64)&&(a<98)
+{
+printf("enter ch is alpha");
+}
+else
+{printf(enter ch is not a alph");
+}
+getch();
+}
