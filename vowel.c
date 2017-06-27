6e6585d460ff0c41e5f7d3086cab1b3972769a04
boopathytekh <boopathytekh@gmail.com>
2017-06-27 17:13:32 +0530

    vowel.c

---
+++ vowel.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+void  main()
+{
+    char c;
+    int isLowercaseVowel, isUppercaseVowel;
+    clrscr();
+
+    printf("Enter an alphabet: ");
+    scanf("%c",&c);
+
+       isLowercaseVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+
+    
+    isUppercaseVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+
+    
+    if (isLowercaseVowel || isUppercaseVowel)
+        printf("%c is a vowel.", c);
+    else
+        printf("%c is a consonant.", c);
+   getch();
+}
