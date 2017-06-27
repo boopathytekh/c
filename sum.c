805593aa030a3553a07835f94253cb8d79978bd6
boopathytekh <boopathytekh@gmail.com>
2017-06-27 17:03:32 +0530

    sum.c

---
+++ sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int  i,a;
+int sum;
+scanf("%d",&n);
+clrscr();
+for(i=0;i>n;,i++)
+{
+sum=sum+n;
+}
+printf("the sum of digits=%d",sum);
+getch();
+}
