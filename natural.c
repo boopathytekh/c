fa0be787cfde393a1a864a0a9842b8745193a0e6
boopathytekh <boopathytekh@gmail.com>
2017-06-28 11:07:21 +0530

    natural

---
+++ natural.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,sum;
+clrscr();
+scanf("%d",&a);
+for(a=1;a<=10;a++)
+{
+sum=sum+a;
+}
+printf("the sum of natural no=%",sum);
+getch();
+}
+
